e383898b4417c5f25ab89408107de42468aef2b7
LydRod206 <119384486+LydRod206@users.noreply.github.com>
2023-05-04 17:40:08 -0700

    models

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,7 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
+USE ecommerce_db;
 
 -- CREATE CATEGORY MODEL
 CREATE TABLE Categories (
@@ -16,7 +17,7 @@ CREATE TABLE Products (
     price DECIMAL(10,2) NOT NULL, 
     stock INT NOT NULL DEFAULT 10,
     category_id INT UNSIGNED NOT NULL,
-    CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES categories(id)
+    CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES Categories(id)
 );
 -- CREATE TAG MODEL
 CREATE TABLE Tags (
@@ -24,15 +25,10 @@ CREATE TABLE Tags (
     tag_name VARCHAR(255)
 );
 -- CREATE PRODUCTTAG THROUGH MODEL
-CREATE TABLE ProductTag (
+CREATE TABLE ProductTags (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     product_id INT UNSIGNED NOT NULL,
     CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES Products(id),
     tag_id INT UNSIGNED NOT NULL,
     CONSTRAINT fk_tag_id FOREIGN KEY (tag_id) REFERENCES Tags(id)
 );
--- CREATE ASSOCIATIONS BETWEEN MODELS / JUNCTION TABLE
-Categories.hasMany(Products);
-Products.belongsTo(Categories);
-Products.belongsToMany(Tags, {through: ProductTags});
-Tags.belongsToMany(Products, {through: ProductTag});
